008d541b9ca1a9827b6512fab5132f8706b4790c
Piero Viscone <pieroviscone@gmail.com>
2022-12-09 21:36:35 +0100

    ttbarKinematics/madgraph: histutils porting to class

--- tasks/ttbarKinematics/madgraph/HistUtils.h
+++ tasks/ttbarKinematics/madgraph/HistUtils.h
@@ -19,221 +19,322 @@
 
 using namespace ROOT;
 using namespace ROOT::Math;
+using namespace ROOT::RDF;
 
-//----------------------------------------------------------------------
-//----------------------------------------------------------------------
-//                         HISTS
-// TODO Generalize this function to accept any number of histograms (vector of histograms)
-/**
- * @brief Function that creates, save and return a THStack object of two histograms
- *
- * @param histVector Vector of histograms to add to the stack
- * @param title Title of the final plot
- * @param xLabel Label of the x axis
- * @param savePath Path/filename.png where the plot will be saved
- * @param ratio If true, the plot will have a ratio plot
- * @param fit If true, the plot will have a Breit-Wigner fit
- * @param log If true, the plot will be in log YScale
- * @return THStack* Return the THStack object
- */
-THStack *StackHist(std::vector<TH1 *> histVector, std::string title, std::string xLabel, std::string savePath = "", bool ratio = false, bool fit = false, bool log = false) {
 
-    //-------------------------DECLARATIONS--------------------------------
-    // Set the colors,size and the stat options
-    gStyle->SetOptStat(00011110);
 
-    // TODO Add other colors
-    std::vector<int> histColor{798, 920};
-    std::vector<double> alphaColor{1., 0.3};
-    std::vector<int> lineColor{798, 1};
-    std::vector<int> markerColor{1, 1};
-    std::vector<double> markerSize{0, 0.5};
-    std::vector<int> fitColor{
-        2,
-        9,
-    };
+
+
+
+
+//TODO Create different TStyle (or palettes) and apply them
+int stackPlotterCounter = 0;
+class StackPlotter {
+private:
+    std::vector<RResultPtr<::TH1D>> RResultVector;
+    Int_t palette = 70;
     int fitWidth = 2;
-
     int nStatBox = 3;
-
-    std::string drawHSOption = "nostack hist";
-
-    TCanvas *c = new TCanvas("cName", "cName", 50, 50, 800, 700);
-    THStack *hs = new THStack("hs", title.c_str());
-
-    TPad *p1;
-    TPad *p2;
-    std::vector<TPaveStats *> statsVector(nStatBox);
-    std::vector<TF1 *> tfVector(histVector.size());
-
-    if (ratio) {
-        if (histVector.size() != 2) {
-            std::cerr << "ERROR: Ratio plot can be done only with two histograms" << std::endl;
-            ratio = false;
-        } else {
-            p1 = new TPad("pad1", "pad1", 0, 0.3, 0.95, 0.98);
-            p2 = new TPad("pad2", "pad2", 0, 0.05, 0.95, 0.3);
-            p1->Draw();
-            p2->Draw();
-
-            // First pad: THStack
-            p1->cd();
-            p1->SetBottomMargin(0.); // Upper and lower plot are joined
-        }
-    }
-    if (!ratio) {
-        p1 = new TPad("pad1", "pad1", 0, 0.00, 0.95, 1);
-        p2 = nullptr;
-        p1->Draw();
-        p1->cd();
-    }
-    TLegend *legend = new TLegend(0.79, 0.62, 0.92, 0.74);
-
-    //---------------------LOOP OVER HISTOGRAMS--------------------------------
-    p1->cd();
-    for (int idx = 0; idx < histVector.size(); idx++) {
-
-        if (!log) {
-            histVector[idx]->GetYaxis()->SetRangeUser(0, 1.15 * std::max(histVector[idx]->GetMaximum(), histVector[idx]->GetMaximum()));
-        } else {
-            histVector[idx]->GetYaxis()->SetRangeUser(0.1, 1.15 * std::max(histVector[idx]->GetMaximum(), 11.5 * histVector[idx]->GetMaximum()));
-            gPad->SetLogy();
-        }
-
-        histVector[idx]->SetFillColorAlpha(histColor[idx], alphaColor[idx]);
-        histVector[idx]->SetLineColor(lineColor[idx]);
-        histVector[idx]->SetMarkerColor(markerColor[idx]);
-        histVector[idx]->SetMarkerSize(markerSize[idx]);
-        hs->Add(histVector[idx], "sames");
-
-        hs->Draw(drawHSOption.c_str());
-        gPad->Update();
-
-        legend->AddEntry(histVector[idx]->GetName(), histVector[idx]->GetTitle(), "f");
-
-        if (fit) {
-            std::string funcName = "f" + std::to_string(idx);
-            TF1 *f = new TF1(funcName.c_str(), "breitwigner", histVector[idx]->GetXaxis()->GetXmin(), histVector[idx]->GetXaxis()->GetXmax());
-
-            f->SetParameters(histVector[idx]->GetMaximum(), histVector[idx]->GetMean(), histVector[idx]->GetRMS());
-            histVector[idx]->Fit(funcName.c_str(), "L 0");
-
-            f->SetNpx(500);
-            f->SetLineColor(fitColor[idx]);
-            f->SetLineWidth(fitWidth);
-
-            tfVector[idx] = f;
-        }
-        // TODO Make the stats box bigger
-        if (histVector.size() <= nStatBox) {
-            statsVector[idx] = (TPaveStats *)histVector[idx]->GetListOfFunctions()->FindObject("stats");
-            statsVector[idx]->SetX1NDC(.78 - idx * 0.18);
-            statsVector[idx]->SetX2NDC(0.95 - idx * 0.18);
-            statsVector[idx]->SetY1NDC(.78);
-            statsVector[idx]->SetY2NDC(.92);
-            statsVector[idx]->SetLineColor(histColor[idx]);
-            c->Modified();
-        }
-    }
-
-    gPad->Update();
-    if (fit) {
-        int idx = 0;
-        for (auto &&func : tfVector) {
-            func->Draw("same");
-            std::string funcName = "f" + std::to_string(idx);
-            std::string legFit = histVector[idx]->GetTitle();
-            legFit += " BW fit";
-            legend->AddEntry(funcName.c_str(), legFit.c_str(), "l");
-            idx++;
-        }
-    }
-
-    hs->GetYaxis()->SetTitle("Events");
-    hs->GetYaxis()->SetLabelOffset(0.01);
-
-    // Title
-    TLatex TeX;
-    TeX.SetTextFont(42);
-    TeX.SetTextSize(0.038);
-    TeX.SetTextAlign(21);
-    TeX.DrawLatexNDC(0.55, 0.97, title.c_str());
-
-    // Legend
-    legend->SetBorderSize(0);
-    legend->Draw();
-
-    // Draw CMS on top left
     int iPos = 11;
-    float iPeriod = 0;
-    CMS_lumi(c, iPeriod, iPos);
+    float statGap = 0.18;
+    std::vector<float> legendPos{0.79, 0.62, 0.92, 0.74};
+    std::vector<float> statPos{.78, .78, .95, .92};
+    std::string drawOpt = "nostack hist PLC";
+    TCanvas *c;
+    std::vector<TH1 *> histVector;
+    THStack *hs;
+    std::string title;
+    std::string xLabel;
+    bool ratio = false;
+    bool log = false;
+    bool fit = false;
+    bool normalize = false;
+    std::string savePath;
+    bool constructed = false;
+    std::string canvasName = "cName";
 
-    //---------------------------RATIO PLOT--------------------------------
-    if (ratio) {
-        // Thing to avoid the overlap on the first label
-        histVector[0]->GetYaxis()->SetLabelSize(0.);
-        TAxis *axis = hs->GetYaxis();
-        axis->ChangeLabel(1, -1, -1, -1, -1, -1, " ");
-        axis->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-        axis->SetLabelSize(15);
-
-        // Ratio plot
-        p2->cd();
-        p2->SetTopMargin(0);
-        p2->SetBottomMargin(0.2);
-        TH1F *histRatio = (TH1F *)histVector[0]->Clone("histRatio");
-        histRatio->Divide(histVector[1]);
-
-        // Ratio plot options
-        histRatio->SetStats(0);
-        histRatio->SetTitle("");
-        histRatio->SetLineColor(kBlack);
-        histRatio->SetMarkerColor(kBlack);
-        histRatio->SetMarkerSize(0.5);
-        histRatio->Draw();
-        gPad->Update();
-
-        // Y axis ratio plot settings
-        histRatio->GetYaxis()->SetRangeUser(-1.2, 3.2);
-        histRatio->GetYaxis()->SetTitle("Ratio");
-        histRatio->GetYaxis()->SetNdivisions(505);
-        histRatio->GetYaxis()->SetTitleSize(20);
-        histRatio->GetYaxis()->SetTitleFont(43);
-        histRatio->GetYaxis()->SetTitleOffset(1.55);
-        histRatio->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-        histRatio->GetYaxis()->SetLabelSize(15);
-
-        // X axis ratio plot settings
-        histRatio->GetXaxis()->SetTitleSize(20);
-        histRatio->GetXaxis()->SetTitleFont(43);
-        histRatio->GetXaxis()->SetTitleOffset(0.75);
-        histRatio->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-        histRatio->GetXaxis()->SetLabelSize(15);
-        gPad->Modified();
-        gPad->Update();
-        c->RedrawAxis();
-
-        // Red line on ratio=1
-        TLine *line = new TLine(p2->GetUxmin(), 1., p2->GetUxmax(), 1.);
-        line->SetLineColor(kRed);
-        line->Draw();
-        if (log) {
-            histVector[0]->GetYaxis()->SetRangeUser(0.1, 1.15 * std::max(histVector[0]->GetMaximum(), histVector[1]->GetMaximum()));
-            p1->SetLogy();
-        }
-    } else {
-        hs->GetXaxis()->SetTitle(xLabel.c_str());
+public:
+    StackPlotter(std::vector<RResultPtr<::TH1D>> RResultVector, std::string title, std::string xLabel, std::string savePath = "", bool ratio = false, bool fit = false, bool log = false) {
+        canvasName += std::to_string(stackPlotterCounter);
+        stackPlotterCounter++;
+        this->RResultVector = RResultVector;
+        this->savePath = savePath;
+        this->title = title;
+        this->xLabel = xLabel;
+        this->ratio = ratio;
+        this->fit = fit;
+        this->log = log;
+    };
+    ~StackPlotter(){};
+    void Normalize() {
+        this->normalize = true;
     }
+    void SetIPos(int iPos) {
+        this->iPos = iPos;
+    };
+    void SetLegendPos(std::vector<float> legendPos) {
+        this->legendPos = legendPos;
+    };
+    void SetLegendPos(int idx, float pos) {
+        this->legendPos[idx] = pos;
+    };
+    void SetStatPos(std::vector<float> statPos) {
+        this->statPos = statPos;
+    };
+    void SetStatPos(int idx, float pos) {
+        this->statPos[idx] = pos;
+    };
+    void SetStatGap(float statGap) {
+        this->statGap = statGap;
+    };
+    void SetSavePath(std::string savePath) {
+        this->savePath = savePath;
+    };
+    void SetLog(bool log) {
+        this->log = log;
+    };
+    void SetFit(bool fit) {
+        this->fit = fit;
+    };
+    void SetRatio(bool ratio) {
+        this->ratio = ratio;
+    };
+    void SetNStatBox(int nStatBox) {
+        this->nStatBox = nStatBox;
+    };
+    void SetFitWidth(int fitWidth) {
+        this->fitWidth = fitWidth;
+    };
+    void SetDrawOpt(std::string drawOpt) {
+        this->drawOpt = drawOpt;
+    };
+    void SetPalette(Int_t palette) {
+        this->palette = palette;
+    };
+    THStack *GetStack() {
+        return hs;
+    };
+    TCanvas *GetCanvas() {
+        return c;
+    };
+    void GetValue() {
+        c = new TCanvas(canvasName.c_str(), canvasName.c_str(), 50, 50, 800, 700);
+        hs = new THStack("hs", title.c_str());
+        for (auto &resultptr : RResultVector) {
+            TH1D hist = resultptr.GetValue();
+            TH1D *histptr = new TH1D;
+            *histptr = hist;
+            histVector.push_back(histptr);
+        }
+        constructed = true;
+    }
+    void SetBinLabel(int idx, std::string label) {
+        if (!constructed) {
+            GetValue();
+        }
+        for (auto &&hist : histVector) {
+            hist->GetXaxis()->SetBinLabel(idx, label.c_str());
+        }
+    }
+    void Draw() {
 
-    // Save and return
-    gPad->Modified();
-    c->Update();
-    c->RedrawAxis();
-    c->GetFrame()->Draw();
+        if (!constructed) {
+            GetValue(); 
+        }
+        c->cd();
 
-    if (savePath != "") {
+        //TODO REMOVE THIS, now are used only in TF1 and TPaveStats
+        std::vector<int> histColor{798, 920};
+        std::vector<double> alphaColor{1., 0.3};
+        std::vector<int> lineColor{798, 1};
+        std::vector<int> markerColor{1, 1};
+        std::vector<double> markerSize{0, 0.5};
+        std::vector<int> fitColor{
+            2,
+            9,
+        };
+
+        if (histVector.size() > nStatBox) {
+            gStyle->SetOptStat(00000000);
+        } else {
+            gStyle->SetOptStat(00011110);
+        }
+
+
+        gStyle->SetPalette(70);
+
+        TPad *p1;
+        TPad *p2;
+        std::vector<TPaveStats *> statsVector(nStatBox);
+        std::vector<TF1 *> tfVector(histVector.size());
+        std::string padName = "pad";
+        padName += stackPlotterCounter;
+        if (ratio) {
+            if (histVector.size() != 2) {
+                std::cerr << "ERROR: Ratio plot can be done only with two histograms" << std::endl;
+                ratio = false;
+            } else {
+                p1 = new TPad((padName + "_1").c_str(), (padName + "_1").c_str(), 0, 0.3, 0.95, 0.98);
+                p2 = new TPad((padName + "_2").c_str(), (padName + "_2").c_str(), 0, 0.05, 0.95, 0.3);
+                p1->Draw();
+                p2->Draw();
+
+                // First pad: THStack
+                p1->cd();
+                p1->SetBottomMargin(0.); // Upper and lower plot are joined
+            }
+        } else {
+            p1 = new TPad((padName + "_1").c_str(), (padName + "_1").c_str(), 0, 0.00, 0.95, 1);
+            p2 = nullptr;
+            p1->Draw();
+            p1->cd();
+        }
+        TLegend *legend = new TLegend(legendPos[0], legendPos[1], legendPos[2], legendPos[3]);
+        p1->cd();
+        for (int idx = 0; idx < histVector.size(); idx++) {
+            if (normalize) {
+                histVector[idx]->Scale(1. / histVector[idx]->Integral(), "width");
+            }
+            if (!log) {
+                histVector[idx]->GetYaxis()->SetRangeUser(0, 1.15 * std::max(histVector[idx]->GetMaximum(), histVector[idx]->GetMaximum()));
+            } else {
+                histVector[idx]->GetYaxis()->SetRangeUser(0.1, 1.15 * std::max(histVector[idx]->GetMaximum(), 11.5 * histVector[idx]->GetMaximum()));
+                gPad->SetLogy();
+            }
+
+            hs->Add(histVector[idx], "sames");
+
+            hs->Draw(drawOpt.c_str());
+            gPad->Update();
+
+            legend->AddEntry(histVector[idx]->GetName(), histVector[idx]->GetTitle(), "f");
+
+            if (fit) {
+                std::string funcName = "f" + std::to_string(idx);
+                TF1 *f = new TF1(funcName.c_str(), "breitwigner", histVector[idx]->GetXaxis()->GetXmin(), histVector[idx]->GetXaxis()->GetXmax());
+
+                f->SetParameters(histVector[idx]->GetMaximum(), histVector[idx]->GetMean(), histVector[idx]->GetRMS());
+                histVector[idx]->Fit(funcName.c_str(), "L 0");
+
+                f->SetNpx(500);
+                f->SetLineColor(fitColor[idx]);
+                f->SetLineWidth(fitWidth);
+
+                tfVector[idx] = f;
+            }
+
+            if (histVector.size() <= nStatBox) {
+                statsVector[idx] = (TPaveStats *)histVector[idx]->GetListOfFunctions()->FindObject("stats");
+                statsVector[idx]->SetX1NDC(statPos[0] - idx * statGap);
+                statsVector[idx]->SetY1NDC(statPos[1]);
+                statsVector[idx]->SetX2NDC(statPos[2] - idx * statGap);
+
+                statsVector[idx]->SetY2NDC(statPos[3]);
+                statsVector[idx]->SetLineColor(histColor[idx]);
+                c->Modified();
+            }
+        }
+        gPad->Update();
+        if (fit) {
+            int idx = 0;
+            for (auto &&func : tfVector) {
+                func->Draw("same");
+                std::string funcName = "f" + std::to_string(idx);
+                std::string legFit = histVector[idx]->GetTitle();
+                legFit += " BW fit";
+                legend->AddEntry(funcName.c_str(), legFit.c_str(), "l");
+                idx++;
+            }
+        }
+
+        hs->GetYaxis()->SetTitle("Events");
+        hs->GetYaxis()->SetLabelOffset(0.01);
+
+        // Title
+        TLatex TeX;
+        TeX.SetTextFont(42);
+        TeX.SetTextSize(0.038);
+        TeX.SetTextAlign(21);
+        TeX.DrawLatexNDC(0.55, 0.97, title.c_str());
+
+        // Legend
+        legend->SetBorderSize(0);
+        legend->Draw();
+
+        // Draw CMS on top left
+
+        float iPeriod = 0;
+        CMS_lumi(c, iPeriod, iPos);
+
+        //---------------------------RATIO PLOT--------------------------------
+        if (ratio) {
+            // Thing to avoid the overlap on the first label
+            histVector[0]->GetYaxis()->SetLabelSize(0.);
+            TAxis *axis = hs->GetYaxis();
+            axis->ChangeLabel(1, -1, -1, -1, -1, -1, " ");
+            axis->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+            axis->SetLabelSize(15);
+
+            // Ratio plot
+            p2->cd();
+            p2->SetTopMargin(0);
+            p2->SetBottomMargin(0.2);
+            TH1F *histRatio = (TH1F *)histVector[0]->Clone("histRatio");
+            histRatio->Divide(histVector[1]);
+
+            // Ratio plot options
+            histRatio->SetStats(0);
+            histRatio->SetTitle("");
+            histRatio->SetLineColor(kBlack);
+            histRatio->SetMarkerColor(kBlack);
+            histRatio->SetMarkerSize(0.5);
+            histRatio->Draw();
+            gPad->Update();
+
+            // Y axis ratio plot settings
+            histRatio->GetYaxis()->SetRangeUser(-1.2, 3.2);
+            histRatio->GetYaxis()->SetTitle("Ratio");
+            histRatio->GetYaxis()->SetNdivisions(505);
+            histRatio->GetYaxis()->SetTitleSize(20);
+            histRatio->GetYaxis()->SetTitleFont(43);
+            histRatio->GetYaxis()->SetTitleOffset(1.55);
+            histRatio->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+            histRatio->GetYaxis()->SetLabelSize(15);
+
+            // X axis ratio plot settings
+            histRatio->GetXaxis()->SetTitleSize(20);
+            histRatio->GetXaxis()->SetTitleFont(43);
+            histRatio->GetXaxis()->SetTitleOffset(0.75);
+            histRatio->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+            histRatio->GetXaxis()->SetLabelSize(15);
+            gPad->Modified();
+            gPad->Update();
+            c->RedrawAxis();
+
+            // Red line on ratio=1
+            TLine *line = new TLine(p2->GetUxmin(), 1., p2->GetUxmax(), 1.);
+            line->SetLineColor(kRed);
+            line->Draw();
+            if (log) {
+                histVector[0]->GetYaxis()->SetRangeUser(0.1, 1.15 * std::max(histVector[0]->GetMaximum(), histVector[1]->GetMaximum()));
+                p1->SetLogy();
+            }
+        } else {
+            hs->GetXaxis()->SetTitle(xLabel.c_str());
+        }
+        // Save and return
+        gPad->Modified();
+        c->Update();
+        c->RedrawAxis();
+        c->GetFrame()->Draw();
+    };
+
+    void Save(std::string path) {
+        Draw();
+        c->SaveAs(path.c_str());
+    }
+    void Save() {
+        Draw();
         c->SaveAs(savePath.c_str());
     }
-
-    return hs;
-}
+};
