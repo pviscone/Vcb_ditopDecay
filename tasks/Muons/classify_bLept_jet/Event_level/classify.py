7d43cd51de5381a18c8b64dd011334194d833cab
Piero Viscone <pieroviscone@gmail.com>
2023-03-10 19:07:15 +0100

    Dropped some variables from training

--- tasks/Muons/classify_bLept_jet/Event_level/classify.py
+++ tasks/Muons/classify_bLept_jet/Event_level/classify.py
@@ -40,28 +40,39 @@ df = pd.read_pickle("./event_df.pkl", compression="bz2")
 
 label=np.expand_dims(df["label"].astype(float).to_numpy(), axis=1)
 df=df.loc[:, df.columns != "label"]
+df=df.drop(columns=["Muon_mass", "Neutrino_mass"])
 
+
+"""
 ohe = OneHotEncoder()
 ohe.fit(label)
 label = ohe.transform(label).toarray()
+"""
 
 data = torch.tensor(df.to_numpy(), dtype=torch.float32, device=device)
-label = torch.tensor(label, dtype=torch.float32, device=device)
+label = torch.tensor(label, dtype=torch.long, device=device)
 
 train_data,test_data,train_label,test_label=train_test_split(data,label, test_size=0.2, shuffle=True)
 
 #%%
+
+importlib.reload(MLP_model)
+MLP = MLP_model.MLP
+
+
 model = MLP(x_train=train_data, y_train=train_label, x_test=test_data, y_test=test_label,
-            hidden_arch=[100,100], batch_size=10000,
-            optim={"lr": 0.002, "weight_decay": 0.0001, }
+            hidden_arch=[20,20,20], batch_size=1000,
+            optim={"lr": 0.0005, "weight_decay": 0.001, },
+            early_stopping=None
             )
 model = model.to(device)
 
 #! SOMETHIMG IS BROKEN
 #!---------------------Training---------------------
-model.train_loop(epochs=1000)
+
+model.train_loop(epochs=100)
 
 #!---------------------Plot loss---------------------
-#model.loss_plot()
+model.loss_plot()
 
 # %%
