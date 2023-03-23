6494c3ae35312738760c3252691e5b9e6b8346d8
Piero Viscone <pieroviscone@gmail.com>
2023-03-23 20:40:15 +0100

    Added all the labels

--- tasks/Muons/classify_bLept_jet/Event_level/save_event_dataframe.py
+++ tasks/Muons/classify_bLept_jet/Event_level/save_event_dataframe.py
@@ -53,6 +53,12 @@ def pad_and_alternate(obj,variable_list,pad):
             col_labels.append(f"{obj}{i}_{var}")
     return alternate_column(matrix_list), col_labels
 
+def np_and(*args):
+    res=args[0]
+    for arg in args[1:]:
+        res=np.bitwise_and(arg, args[0])
+    return res
+
 nu_pz = np.load("../../neutrino_reco/nu_pz.npy")
 nu_pt = events.MET.pt.to_numpy()
 nu_phi = events.MET.phi.to_numpy()
@@ -74,64 +80,136 @@ del nu_pt, nu_eta, nu_phi, nu_pz
 
 #%%
 
+#* LHE Mask
 #Select only the first product of the W decay
 pdgId_Wdecay= events.LHEPart.pdgId[:,[3,6]]
 #Mask for the leptonic decay of the W
 leptonic_LHE_mask=np.bitwise_or(pdgId_Wdecay==13,pdgId_Wdecay==-13)
 hadronic_LHE_mask=np.bitwise_not(leptonic_LHE_mask)
 
+#*Define Jet objects
 
-bLept_LHE=events.LHEPart[:,[2,5]][leptonic_LHE_mask]
 near_Jet,deltaR = events.LHEPart.nearest(events.Jet,return_metric=True)
 bLept_Jet = near_Jet[:, [2, 5]][leptonic_LHE_mask]
+bHad_Jet = near_Jet[:, [2, 5]][hadronic_LHE_mask]
+Wb_Jet= near_Jet[:, [4, 6]][hadronic_LHE_mask]
+Wc_Jet= near_Jet[:, [3, 7]][hadronic_LHE_mask]
 
+
+#! Replace W1,W2 with the cW, bW
+
+#*Compute the deltaR between the LHE and the Jet
 deltaRLept=deltaR[:, [2, 5]][leptonic_LHE_mask]
 deltaRHad=deltaR[:, [2, 5]][hadronic_LHE_mask]
-deltaRW1=deltaR[:, [3, 6]][hadronic_LHE_mask]
-deltaRW2=deltaR[:, [4, 7]][hadronic_LHE_mask]
+deltaRWb=deltaR[:, [4, 6]][hadronic_LHE_mask]
+deltaRWc=deltaR[:, [3, 7]][hadronic_LHE_mask]
 
-plt.title("Order in pt of the bLept jet")
-bLept_pt_order = ak.argmax(events.Jet.pt == bLept_Jet.pt, axis=1)
-plt.hist(bLept_pt_order)
+
+# * Apply the mask
+Rmask = np_and(deltaRLept < 0.4,
+              deltaRHad < 0.4,
+              deltaRWb < 0.4,
+              deltaRWc < 0.4,
+              )
+
+#*Compute the order of the jets in pt and compute the efficiency of selecting the first N
+
+n_ev=len(bLept_Jet[Rmask])
+bLept_pt_order = ak.argmin(events.Jet.delta_r(bLept_Jet), axis=1)
+bHad_pt_order = ak.argmin(events.Jet.delta_r(bHad_Jet), axis=1)
+Wb_pt_order = ak.argmin(events.Jet.delta_r(Wb_Jet), axis=1)
+Wc_pt_order = ak.argmin(events.Jet.delta_r(Wc_Jet), axis=1)
+
+plt.figure()
+
+same_match = np.array([ak.sum(bLept_pt_order[Rmask] == bHad_pt_order[Rmask]),
+                       ak.sum(bLept_pt_order[Rmask] == Wb_pt_order[Rmask]),
+                       ak.sum(bLept_pt_order[Rmask] == Wc_pt_order[Rmask]),
+                       ak.sum(bHad_pt_order[Rmask] == Wb_pt_order[Rmask]),
+                       ak.sum(bHad_pt_order[Rmask] == Wc_pt_order[Rmask]),
+                       ak.sum(Wb_pt_order[Rmask] == Wc_pt_order[Rmask])
+])
+same_match=same_match/n_ev
+lab=["bLept==bHad","bLept==Wb","bLept==Wc","bHad==Wb","bHad==Wc","Wb==Wc"]
+
+plt.bar(lab,width=same_match)
+
+same_match_matrix=np.array([(bLept_pt_order[Rmask] == bHad_pt_order[Rmask]),
+                    (bLept_pt_order[Rmask] == Wb_pt_order[Rmask]),
+                    (bLept_pt_order[Rmask] == Wc_pt_order[Rmask]),
+                    (bHad_pt_order[Rmask] == Wb_pt_order[Rmask]),
+                    (bHad_pt_order[Rmask] == Wc_pt_order[Rmask]),
+                    (Wb_pt_order[Rmask] == Wc_pt_order[Rmask])]).T
+
+print(f"Fraction of event with at least 2 jet matching to the same parton: {np.bitwise_or.reduce(same_match_matrix, 1).sum()/n_ev}")
+
+assert ak.sum(bLept_pt_order==bHad_pt_order)==0
+assert ak.sum(bLept_pt_order==Wb_pt_order)==0
+assert ak.sum(bLept_pt_order==Wc_pt_order)==0
+assert ak.sum(bHad_pt_order==Wb_pt_order)==0
+assert ak.sum(bHad_pt_order==Wc_pt_order)==0
+assert ak.sum(Wb_pt_order==Wc_pt_order)==0
+
+
+n_max=15
+plt.title("Jet pt order")
+plt.hist(bLept_pt_order,label="bLept",histtype="step",range=(0,n_max),bins=n_max)
+plt.hist(bHad_pt_order,label="bHad",histtype="step",range=(0,n_max),bins=n_max)
 plt.yscale("log")
-plt.xlabel("Order in pt")
+plt.xlabel("Jet Order in pt")
+plt.legend()
 
 
-num_jet_to_select=7
-efficiency = len(bLept_pt_order[np.bitwise_and(bLept_pt_order <
-                 num_jet_to_select,deltaRLept<0.4)])/len(bLept_pt_order[deltaRLept<0.4])
-print(f"Percentage of events with the bLept jet in the first {num_jet_to_select} jets:{efficiency}")
+num_jet_to_select=6
 
-mask=np.bitwise_and(deltaRLept<0.4, deltaRHad<0.4)
-mask=np.bitwise_and(mask, deltaRW1<0.4)
-mask=np.bitwise_and(mask, deltaRW2<0.4)
-mask= np.bitwise_and(mask, bLept_pt_order < num_jet_to_select)
+
+mask=np_and(Rmask,
+            bLept_pt_order < num_jet_to_select,
+            bHad_pt_order < num_jet_to_select,
+            Wb_pt_order < num_jet_to_select,
+            Wc_pt_order < num_jet_to_select,)
+
+
+efficiency = ak.sum(mask)/ak.sum(Rmask)
+print(f"Percentage of events with jets in the first {num_jet_to_select} jets:{efficiency}")
+
+
+
+
 
 #%%
+#*Mask the objects
 events.Jet=events.Jet[:, :num_jet_to_select]
 events = events[mask]
 events.Neutrino = nu_4Vect[mask]
-bLept_LHE = bLept_LHE[mask]
+
 bLept_Jet = bLept_Jet[mask]
+bHad_Jet = bHad_Jet[mask]
 
 
+#*Compute some invariant masses
 events.Muon = events.Muon[:, 0]
 events.Neutrino.Wmass = (events.Muon+events.Neutrino).mass
 events.Tmass = (events.Neutrino+events.Muon+events.Jet).mass
 
 
 #%%
+
+#*Build the matrix
 col_labels=[]
 
 muon_matrix,mu_labels=build_matrix("Muon",["pt","eta","phi","mass"])
 nu_matrix,nu_labels=build_matrix("Neutrino",["pt","eta","phi","mass","Wmass"])
 jet_matrix,jet_labels=pad_and_alternate("Jet",["pt","eta","phi","mass","btagDeepFlavB","btagDeepFlavCvB","btagDeepFlavCvL","area","Tmass"],num_jet_to_select)
 
-label = column(bLept_pt_order[mask])
-col_labels=mu_labels+nu_labels+jet_labels+["label"]
+bLept_label = column(bLept_pt_order[mask])
+bHad_label=column(bHad_pt_order[mask])
+Wb_label=column(Wb_pt_order[mask])
+Wc_label=column(Wc_pt_order[mask])
+col_labels=mu_labels+nu_labels+jet_labels+["bLept_label","bHad_label","Wb_label","Wc_label"]
 
-event_matrix=np.hstack([muon_matrix,nu_matrix,jet_matrix,label])
+event_matrix=np.hstack([muon_matrix,nu_matrix,jet_matrix,bLept_label,bHad_label,Wb_label,Wc_label])
 
 event_df=pd.DataFrame(event_matrix,columns=col_labels)
-event_df.to_pickle("./event_df.pkl", compression="bz2")
+#event_df.to_pickle("./event_df.pkl", compression="bz2")
 # %%
