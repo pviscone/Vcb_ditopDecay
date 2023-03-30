ce9af90878c337f3f7d67d4ac1612aa5ecb9fd2a
pviscone <pieroviscone@gmail.com>
2023-03-30 20:50:24 +0200

    new hyperparameters + jet_mean,std computed only on existing jets

--- tasks/Muons/classify_bLept_jet/Event_level/classify.py
+++ tasks/Muons/classify_bLept_jet/Event_level/classify.py
@@ -21,8 +21,10 @@ device = torch.device(dev)
 
 jets_per_event = 6
 
-df = pd.read_pickle("./event_df.pkl", compression="bz2")
+#df = pd.read_pickle("./event_df.pkl", compression="bz2")
+df=pd.read_pickle("./BigMuons_event_df.pkl")
 
+#%%
 weights=[0.]
 
 
@@ -35,9 +37,8 @@ weights=1/weights
 weights=weights/weights.sum()
 
 
-df=df.loc[:, df.columns != "label"]
+#df=df.loc[:, df.columns != "label"]
 df=df.drop(columns=["Muon_mass", "Neutrino_mass"])
-
 
 
 mu_data=df.loc[:, df.columns.str.contains("Muon")].to_numpy()
@@ -55,6 +56,13 @@ jet_data = torch.tensor(jet_data, dtype=torch.float32, device=device)
 label = torch.tensor(label, dtype=torch.long, device=device)
 
 
+
+temp_jet_df=pd.DataFrame(torch.flatten(jet_data,end_dim=1).to(cpu).numpy())
+jet_mean=torch.tensor(temp_jet_df[temp_jet_df!=0].mean().to_numpy(),dtype=torch.float32, device=device)
+jet_std=torch.tensor(temp_jet_df[temp_jet_df!=0].std().to_numpy(),dtype=torch.float32, device=device)
+
+del temp_jet_df
+
 # %%
 importlib.reload(JPA)
 JPANet = JPA.JPANet
@@ -64,22 +72,23 @@ nu_feat=nu_data.shape[2]
 jet_feat=jet_data.shape[2]
 
 model =JPANet(mu_data=mu_data,nu_data=nu_data,jet_data=jet_data,label=label,test_size=0.15,weight=None,
-            mu_arch=None,nu_arch=None,jet_arch=[jet_feat,100,50],
+            mu_arch=None,nu_arch=None,jet_arch=[jet_feat,50,100],
             attention_arch=[100,50],
-            event_arch=[mu_feat+nu_feat,100,50],
+            event_arch=[mu_feat+nu_feat,50,50],
             prefinal_arch=None,
             final_attention=True,
             final_arch=[100,50],
-            batch_size=20000, n_heads=2, dropout=0.2,
-            optim={"lr": 0.01, "weight_decay": 0.00, },
+            batch_size=20000, n_heads=2, dropout=0.1,
+            optim={"lr": 0.008, "weight_decay": 0.00, },
             early_stopping=None,shuffle=False,
+            jet_mean=jet_mean,jet_std=jet_std,
             )
 model = model.to(device)
 print(f"Number of parameters: {model.n_parameters()}")
 
 #!---------------------Training---------------------
-
-model.train_loop(epochs=500,show_each=20)
+torch.compile(model)
+model.train_loop(epochs=500,show_each=12)
 
 #!---------------------Plot loss---------------------
 model.loss_plot()
