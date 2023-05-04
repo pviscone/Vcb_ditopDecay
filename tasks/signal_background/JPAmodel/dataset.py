d996af914ca9611e84f36be9c5f7337eea2cd9d2
pviscone <pieroviscone@gmail.com>
2023-05-04 18:47:07 +0200

    Refactoring + powheg dataset

--- tasks/signal_background/JPAmodel/dataset.py
+++ tasks/signal_background/JPAmodel/dataset.py
@@ -2,7 +2,36 @@ import torch
 from torch.utils.data import Dataset
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 
+
+def build_datasets(df,jets_per_event,test_size=0.15):
+            
+        mu_data=df.filter(regex="Muon.*(pt|eta|phi)").to_numpy()
+        nu_data=df.filter(regex="MET.*(pt|eta|phi)").to_numpy()
+        jet_data=df.filter(regex="Jet.*(pt|eta|phi|btagDeepFlavCvB|btagDeepFlavCvL|Tmass)").to_numpy()
+        label=df.filter(regex="(label).*").astype(int).to_numpy()
+
+
+
+        mu_data=np.reshape(mu_data, (mu_data.shape[0],1, mu_data.shape[1]))
+        nu_data=np.reshape(nu_data, (nu_data.shape[0],1, nu_data.shape[1]))
+        jet_data=np.reshape(jet_data,
+                            (jet_data.shape[0],
+                            jets_per_event,
+                            jet_data.shape[1]//jets_per_event))
+
+        mu_data = torch.tensor(mu_data, dtype=torch.float32)
+        nu_data = torch.tensor(nu_data, dtype=torch.float32)
+        jet_data = torch.tensor(jet_data, dtype=torch.float32)
+        label = torch.tensor(label, dtype=torch.long)
+        if test_size is not None:
+            mu_train, mu_test, nu_train, nu_test,\
+            jet_train,jet_test,y_train,y_test=train_test_split(mu_data,nu_data,jet_data,label,test_size=test_size,shuffle=True)
+            return EventsDataset(mu_train,nu_train,jet_train,y_train),EventsDataset(mu_test,nu_test,jet_test,y_test)
+        else:
+            return EventsDataset(mu_data,nu_data,jet_data,label)
+        
 class EventsDataset(Dataset):
     def __init__(self, mu_data, nu_data, jet_data, label):
         temp_jet_df=pd.DataFrame(torch.flatten(jet_data,end_dim=1).numpy())
--- tasks/signal_background/classify.py
+++ tasks/signal_background/classify.py
@@ -10,7 +10,7 @@ import mplhep
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 sys.path.append("./JPAmodel/")
-from JPAmodel.dataset import EventsDataset
+from JPAmodel.dataset import EventsDataset,build_datasets
 import JPAmodel.JPANet as JPA
 import os
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
@@ -31,40 +31,17 @@ jets_per_event = 7
 
 #df = pd.read_pickle("./event_df.pkl", compression="bz2")
 #df=pd.read_pickle("./event_df.pkl")
-df=pd.read_pickle("../../../root_files/Muons/BigMuons_SB_df.pkl")
+df=pd.read_pickle("../../../root_files/signal_background/BigMuons_SB_df.pkl")
+powheg_df=pd.read_pickle("../../../root_files/signal_background/BigMuons_SB_df.pkl")
+
 
 #%%
 
-
-mu_data=df.filter(regex="Muon.*(pt|eta|phi)").to_numpy()
-nu_data=df.filter(regex="MET.*(pt|eta|phi)").to_numpy()
-jet_data=df.filter(regex="Jet.*(pt|eta|phi|btagDeepFlavCvB|btagDeepFlavCvL|Tmass)").to_numpy()
-label=df.filter(regex="(label).*").astype(int).to_numpy()
-
-
-
-mu_data=np.reshape(mu_data, (mu_data.shape[0],1, mu_data.shape[1]))
-nu_data=np.reshape(nu_data, (nu_data.shape[0],1, nu_data.shape[1]))
-jet_data=np.reshape(jet_data,
-                    (jet_data.shape[0],
-                     jets_per_event,
-                     jet_data.shape[1]//jets_per_event))
-
-mu_data = torch.tensor(mu_data, dtype=torch.float32)
-nu_data = torch.tensor(nu_data, dtype=torch.float32)
-jet_data = torch.tensor(jet_data, dtype=torch.float32)
-label = torch.tensor(label, dtype=torch.long)
-
-
-test_size=0.15
-mu_train, mu_test, nu_train, nu_test,\
-jet_train,jet_test,y_train,y_test=train_test_split(mu_data,nu_data,jet_data,label,test_size=test_size,shuffle=True)
-
-train_dataset=EventsDataset(mu_train,nu_train,jet_train,y_train)
-test_dataset=EventsDataset(mu_test,nu_test,jet_test,y_test)
+train_dataset,test_dataset=build_datasets(df,jets_per_event=7,test_size=0.15)
 #test_dataset.to(device)
 #train_dataset.to(device)
 
+powheg_dataset=build_datasets(powheg_df,jets_per_event=7,test_size=None)
 
 
 
@@ -74,9 +51,9 @@ test_dataset=EventsDataset(mu_test,nu_test,jet_test,y_test)
 importlib.reload(JPA)
 JPANet = JPA.JPANet
 
-mu_feat=mu_data.shape[2]
-nu_feat=nu_data.shape[2]
-jet_feat=jet_data.shape[2]
+mu_feat=3
+nu_feat=3
+jet_feat=5
 
 model = JPANet(weight=None,
                mu_arch=None, nu_arch=None, jet_arch=[jet_feat, 128, 128],
@@ -105,18 +82,23 @@ model.loss_plot()
 
 
 # %%
+
+
 signal_mask=test_dataset.label.squeeze()==1
 bkg_mask=test_dataset.label.squeeze()==0
 score=torch.exp(model.predict(test_dataset,bunch=10)[:,-1])
 signal_score=score[signal_mask]
-bkg_score=score[bkg_mask]
+bkg_score=torch.exp(model.predict(powheg_dataset,bunch=40)[:,-1])
+#bkg_score=score[bkg_mask]
+
 signal_score=signal_score.detach().to(cpu).numpy()
 bkg_score=bkg_score.detach().to(cpu).numpy()
 mplhep.style.use("CMS")
 plt.rc('axes', axisbelow=True)
 
-
-bins=15
+#%%
+bins=20
+score_range=(0,5.5)
 semileptonic_weight=(138e3    #Lumi
                     *832      #Cross section
                     *0.44 #Semileptonic BR
@@ -124,20 +106,24 @@ semileptonic_weight=(138e3    #Lumi
                     )
 signal_weight=np.ones_like(signal_score)*semileptonic_weight*0.518*8.4e-4/(len(signal_score))
 bkg_weight=np.ones_like(bkg_score)*semileptonic_weight*0.5*(1-8.4e-4)/(len(bkg_score))
-binned_signal_score=plt.hist(np.arctanh(signal_score),bins=bins,range=(0,4.5),
+
+
+
+binned_signal_score=plt.hist(np.arctanh(signal_score),bins=bins,range=score_range,
                              color="dodgerblue",edgecolor="blue",
                              histtype="stepfilled",alpha=0.8,linewidth=2,
                              weights=signal_weight,
                              label="Signal")[0]
-binned_bkg_score=plt.hist(np.arctanh(bkg_score),bins=bins,range=(0,4.5),
+binned_bkg_score=plt.hist(np.arctanh(bkg_score),bins=bins,range=score_range,
                           color="red",edgecolor="red",histtype="step",
                           linewidth=2,label="Background",hatch="//",
                           weights=bkg_weight,
          )[0]
 
 
-signal_hist,bin_edges=np.histogram(np.arctanh(signal_score),bins=bins,range=(0,4.5))
-bkg_hist,_=np.histogram(np.arctanh(bkg_score),bins=bins,range=(0,4.5))
+signal_hist,bin_edges=np.histogram(np.arctanh(signal_score),bins=bins,range=score_range)
+bkg_hist,_=np.histogram(np.arctanh(bkg_score),bins=bins,range=score_range)
+
 bin_centers=(bin_edges[1:]+bin_edges[:-1])/2
 plt.errorbar(bin_centers,
              binned_signal_score,
@@ -157,8 +143,9 @@ plt.ylim(1e-1,1e7)
 mplhep.cms.text("Private Work")
 mplhep.cms.lumitext("$138 fb^{-1}$ $(13 TeV)$")
 
-bkg_not_zero=binned_bkg_score!=0
-fom=binned_signal_score[bkg_not_zero]**2/(binned_bkg_score[bkg_not_zero])
+
+zero_bkg=binned_bkg_score==0
+fom=binned_signal_score**2/(binned_bkg_score+binned_signal_score)
 Q=(np.sum(fom[~np.isnan(fom)]))
 print("Q=",Q)
 print(f"Error: {1/np.sqrt(Q)}")
