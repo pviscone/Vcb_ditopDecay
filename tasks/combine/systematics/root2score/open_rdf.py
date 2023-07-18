4afe01310e2a2d867d1e6a49f6741cba894df2cc
Piero Viscone <pieroviscone@gmail.com>
2023-07-18 18:12:08 +0200

    added weights in samples.json

--- tasks/combine/systematics/root2score/open_rdf.py
+++ tasks/combine/systematics/root2score/open_rdf.py
@@ -7,11 +7,31 @@ ROOT.EnableImplicitMT()
 def build_rdf_dict(json_path):
     sample_dict = json.load(open(json_path))
     rdf_dict = {}
+    weight_dict = {"Muons":{},"Electrons":{}}
     for sample in sample_dict:
-        path=sample_dict[sample]
+        
+        
+        
+        path=sample_dict[sample]["path"]
         if path.endswith(".root"):
             file=path
         else:
             file=glob(path+"/*.root")
-        rdf_dict[sample] = ROOT.RDataFrame("Events", file)
-    return rdf_dict
+            
+            
+        #! For the semileptonic you have to add the efficiencies directly here and not in the json
+        if sample=="bkg":
+            rdf_dict["semiLept"]=ROOT.RDataFrame("Events", file).Filter("Sum(abs(LHEPart_pdgId)==15)==0","NoTaus")
+            rdf_dict["semiLeptTau"]=ROOT.RDataFrame("Events", file).Filter("Sum(abs(LHEPart_pdgId)==15)>0","Taus")
+            weight_dict["Muons"]["semiLept"]=sample_dict[sample]["Muons_weight"]*0.193
+            weight_dict["Electrons"]["semiLept"]=sample_dict[sample]["Electrons_weight"]*0.156
+            weight_dict["Muons"]["semiLeptTau"]=sample_dict[sample]["Muons_weight"]*0.0136
+            weight_dict["Electrons"]["semiLeptTau"]=sample_dict[sample]["Electrons_weight"]*0.0095
+        else:
+            rdf_dict[sample] = ROOT.RDataFrame("Events", file)
+            weight_dict["Muons"][sample]=sample_dict[sample]["Muons_weight"]
+            weight_dict["Electrons"][sample]=sample_dict[sample]["Electrons_weight"]
+            
+            
+        
+    return rdf_dict,weight_dict
