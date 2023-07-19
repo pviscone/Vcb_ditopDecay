cd1f9244cee9f77dbb55170fb011411393289fa4
Piero Viscone <pieroviscone@gmail.com>
2023-07-19 14:36:32 +0200

    Moved vary utils to cpp file

--- tasks/combine/systematics/root2score/vary.py
+++ tasks/combine/systematics/root2score/vary.py
@@ -1,57 +1,14 @@
+import os
 import ROOT
 import correctionlib
 correctionlib.register_pyroot_binding()
 
 ROOT.EnableImplicitMT()
+include_path=os.path.join(os.path.dirname(__file__),"vary_utils.h")
 
-
-ROOT.gInterpreter.Declare("""
-    template <typename T>
-    ROOT::RVec<float> evaluate(T cset, std::vector<ROOT::RVec<float>> const& inputs) {
-        int size = inputs[0].size();
-        ROOT::RVec<float> out(size);
-        for(int i = 0; i < size; i++) {
-            std::vector<correction::Variable::Type> in;
-            for(auto const& input : inputs) {
-                in.push_back(input[i]);
-            }
-            out[i] = cset->evaluate(in);
-        }
-        return out;
-    }
-    """)
-
-ROOT.gInterpreter.Declare("""
-    template <typename T, typename S>
-    ROOT::RVec<float> evaluate(T cset, ROOT::RVec<float> const& input,const S &name) {
-        int size = input.size();
-        ROOT::RVec<float> out(size);
-        for(int i = 0; i < size; i++) {
-            out[i] = cset->evaluate({input[i],name});
-        }
-        return out;
-    }
-    """)
-
-
-ROOT.gInterpreter.Declare("""
-    ROOT::RVec<float> TakeIdx(ROOT::RVec<float> const& input,ROOT::RVec<int> const& idxs) {
-        int size = idxs.size();
-        ROOT::RVec<float> out(size);
-        for(int i = 0; i < size; i++) {
-            out[i]=input[idxs[i]];
-        }
-        return out;
-    }
-    """)
-
-json_path="/scratchnvme/pviscone/Vcb_ditopDecay/tasks/combine/systematics/json"
-#!JES
-ROOT.gInterpreter.Declare(f'auto cset = correction::CorrectionSet::from_file("json/jet_jerc.json");')
-ROOT.gInterpreter.Declare('auto JES = cset->at("Summer19UL18_V5_MC_Total_AK4PFchs");')
-ROOT.gInterpreter.Declare('auto JER= cset->at("Summer19UL18_JRV2_MC_ScaleFactor_AK4PFchs");')
-
-
+ROOT.gInterpreter.ProcessLine(f'#include "{include_path}"')
+ROOT.gInterpreter.Declare('auto JES = JERC->at("Summer19UL18_V5_MC_Total_AK4PFchs");')
+ROOT.gInterpreter.Declare('auto JER= JERC->at("Summer19UL18_JRV2_MC_ScaleFactor_AK4PFchs");')
 
 def vary(rdf_dict):
     
---
+++ tasks/combine/systematics/root2score/vary_utils.h
@@ -0,0 +1,36 @@
+template <typename T>
+ROOT::RVec<float> evaluate(T cset, std::vector<ROOT::RVec<float>> const &inputs) {
+    int size = inputs[0].size();
+    ROOT::RVec<float> out(size);
+    for (int i = 0; i < size; i++) {
+        std::vector<correction::Variable::Type> in;
+        for (auto const &input : inputs) {
+            in.push_back(input[i]);
+        }
+        out[i] = cset->evaluate(in);
+    }
+    return out;
+}
+
+template <typename T, typename S>
+ROOT::RVec<float> evaluate(T cset, ROOT::RVec<float> const &input, const S &name) {
+    int size = input.size();
+    ROOT::RVec<float> out(size);
+    for (int i = 0; i < size; i++) {
+        out[i] = cset->evaluate({input[i], name});
+    }
+    return out;
+}
+
+ROOT::RVec<float> TakeIdx(ROOT::RVec<float> const &input, ROOT::RVec<int> const &idxs) {
+    int size = idxs.size();
+    ROOT::RVec<float> out(size);
+    for (int i = 0; i < size; i++) {
+        out[i] = input[idxs[i]];
+    }
+    return out;
+}
+
+auto JERC = correction::CorrectionSet::from_file("json/jet_jerc.json");
+auto btagging = correction::CorrectionSet::from_file("json/btagging.json");
+auto ctagging = correction::CorrectionSet::from_file("json/ctagging.json");
