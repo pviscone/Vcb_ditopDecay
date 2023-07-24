bae1fb9874cf2556db530c9e64ffb127f3e639b3
Piero <pieroviscone@gmail.com>
2023-07-24 18:44:39 +0200

    semiLept devided in semiLeptEle and semiLeptMu

--- tasks/combine/systematics/root2score/open_rdf.py
+++ tasks/combine/systematics/root2score/open_rdf.py
@@ -24,12 +24,15 @@ def build_rdf_dict(sample_dict,bunch_size=10):
         
         
         if sample=="bkg":
-            rdf_dict["semiLept"]=[ROOT.RDataFrame("Events", file[i*bunch_size:(i+1)*bunch_size]).Filter("Sum(abs(LHEPart_pdgId)==15)==0","NoTaus") for i in range(n_bunches)]
+            rdf_dict["semiLeptMu"]=[ROOT.RDataFrame("Events", file[i*bunch_size:(i+1)*bunch_size]).Filter("Sum(abs(LHEPart_pdgId)==13)>0","Muons") for i in range(n_bunches)]
+            rdf_dict["semiLeptEle"]=[ROOT.RDataFrame("Events", file[i*bunch_size:(i+1)*bunch_size]).Filter("Sum(abs(LHEPart_pdgId)==11)>0","Electrons") for i in range(n_bunches)]
             rdf_dict["semiLeptTau"]=[ROOT.RDataFrame("Events", file[i*bunch_size:(i+1)*bunch_size]).Filter("Sum(abs(LHEPart_pdgId)==15)>0","Taus") for i in range(n_bunches)]
-            weight_dict["Muons"]["semiLept"]=sample_dict[sample]["Muons_weight"]
-            weight_dict["Electrons"]["semiLept"]=sample_dict[sample]["Electrons_weight"]
-            weight_dict["Muons"]["semiLeptTau"]=sample_dict[sample]["Muons_weight"]
-            weight_dict["Electrons"]["semiLeptTau"]=sample_dict[sample]["Electrons_weight"]
+            weight_dict["Muons"]["semiLeptMu"]=sample_dict[sample]["Muons_weight"]*0.33
+            weight_dict["Electrons"]["semiLeptMu"]=sample_dict[sample]["Electrons_weight"]*0.33
+            weight_dict["Muons"]["semiLeptEle"]=sample_dict[sample]["Muons_weight"]*0.33
+            weight_dict["Electrons"]["semiLeptEle"]=sample_dict[sample]["Electrons_weight"]*0.33
+            weight_dict["Muons"]["semiLeptTau"]=sample_dict[sample]["Muons_weight"]*0.33
+            weight_dict["Electrons"]["semiLeptTau"]=sample_dict[sample]["Electrons_weight"]*0.33
         else:
             rdf_dict[sample] = [ROOT.RDataFrame("Events", file[i*bunch_size:(i+1)*bunch_size]) for i in range(n_bunches)]
             weight_dict["Muons"][sample]=sample_dict[sample]["Muons_weight"]
