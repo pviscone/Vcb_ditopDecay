Fixed weight variations normalization

tasks/combine/systematics/compute_normWeight.py
@@ -31,22 +31,46 @@ def np_or(*args):
 
     
 
+
+
 f=uproot.open("/scratchnvme/pviscone/Preselection_Skim/powheg/root_files/predict/3A361FB1-E533-1C4D-9EA7-ACFA17F05B69.root")
 
 arrays=f["Events"].arrays(["Jet_pt","Jet_eta","Jet_btagDeepFlavB","Jet_btagDeepFlavCvL","Jet_btagDeepFlavCvB","Jet_hadronFlavour","Jet_jetId","Jet_puId"])
-
+mu_arrays=f["Events"].arrays(["Muon_looseId","Muon_pfIsoId","Muon_pt","Muon_eta"])
 
 arrays=arrays[np_and(
                     arrays["Jet_pt"]>20,
                     np.abs(arrays["Jet_eta"])<2.5,
                     arrays["Jet_jetId"]>0,
-                    arrays["Jet_puId"]>0
+                    arrays["Jet_puId"]>0,
                     )
               ]
-n_ev=1300000
+
+mu_arrays=mu_arrays[np_and(mu_arrays["Muon_looseId"],
+                    mu_arrays["Muon_pfIsoId"]>1,
+                    np.abs(mu_arrays["Muon_eta"])<2.4,)
+]
+
+#arrays=arrays[ak.num(mu_arrays["Muon_pt"])>=1]
+#mu_arrays=mu_arrays[ak.num(mu_arrays["Muon_pt"])>=1]
+#arrays=arrays[mu_arrays["Muon_pt"][:,0]>26]
+#mu_arrays=mu_arrays[mu_arrays["Muon_pt"][:,0]>26]
+
+#arrays=arrays[ak.num(arrays["Jet_pt"])>=4]
+#arrays=arrays[ak.max(arrays["Jet_btagDeepFlavB"],axis=1)>0.27]
+
+n_ev=284000
 arrays=arrays[:n_ev,:7]
+mu_arrays=mu_arrays[:n_ev]
 assert len(arrays)==n_ev
 arrays=ak.pad_none(arrays,7,clip=True,axis=1)
+mu_arrays=ak.pad_none(mu_arrays,1,clip=True,axis=1)
+cuts=ak.fill_none(np_and(ak.num(mu_arrays["Muon_pt"])>=1,
+       mu_arrays["Muon_pt"][:,0]>26,
+       ak.num(arrays["Jet_pt"])>=4
+       ),False)
+
+cuts=ak.fill_none(cuts,False).to_numpy()
 arrays=ak.fill_none(arrays,-1)
 
 
@@ -83,6 +107,7 @@ def evaluate_ctag(array,name):
 
 
 # %%
+#!btag corrections
 b_sources=["hf","lf","hfstats1","hfstats2","lfstats1","lfstats2","cferr1","cferr2"]
 b_systs=[]
 
@@ -129,6 +154,7 @@ print(f"\n\nbTag mean on events (normalized): {mean_prod_b}")
 
 
 #%%
+#!ctag corrections
 c_sources=["Extrap", "Interp", "LHEScaleWeight_muF", "LHEScaleWeight_muR", "PSWeightFSR", "PSWeightISR", "PUWeight", "Stat", "XSec_BRUnc_DYJets_b", "XSec_BRUnc_DYJets_c", "XSec_BRUnc_WJets_c", "jer", "jesTotal"]
 
 c_systs=[]
@@ -146,12 +172,12 @@ for c_syst in c_systs:
 
 ctag_df=ak.to_dataframe(arrays)
 c_systs=["c_"+c_syst for c_syst in c_systs]
-cdf=ctag_df[c_systs+["c_central","Jet_hadronFlavour","Jet_btagDeepFlavB"]]
+cdf=ctag_df[c_systs+["c_central","Jet_hadronFlavour","Jet_btagDeepFlavB","Jet_btagDeepFlavCvB","Jet_btagDeepFlavCvL"]]
 
 
 
 
-
+o_cdf=copy.copy(cdf)
 cdf=cdf[cdf["Jet_hadronFlavour"]!=-1]
 c_cdf=cdf[cdf["Jet_hadronFlavour"]==4]
 b_cdf=cdf[(cdf["Jet_hadronFlavour"]==5)]
@@ -171,7 +197,7 @@ print("\n")
 #%%
 
 print(f"cTag mean on events (not normalized):\n{cdf.groupby(level=0).prod().mean()}")
-
+original_cdf=copy.copy(cdf)
 c_cmu=c_cdf.mean()
 b_cmu=b_cdf.mean()
 l_cmu=l_cdf.mean()
@@ -185,7 +211,7 @@ print(f"\n\ncTag mean on events (normalized): {mean_prod_c}")
 
 
 #%%
-
+#!Plotting
 
 mplhep.style.use("CMS")
 

tasks/combine/systematics/root2score/skim.py
@@ -1,6 +1,6 @@
 import ROOT
 import os
-
+import numpy as np
 
 include_path=os.path.join(os.path.dirname(__file__),"RDF_utils.h")
 
@@ -46,15 +46,17 @@ def jet_selection(df,lepton,weight_syst_list):
             .Redefine(f"Jet_btagDeepFlavCvB",f"pad_jet(Jet_btagDeepFlavCvB[JetMask && JetMatchingMask],7)")
             .Redefine(f"Jet_btagDeepFlavCvL",f"pad_jet(Jet_btagDeepFlavCvL[JetMask && JetMatchingMask],7)")
             .Redefine(f"nJet",f"Sum(JetMask && JetMatchingMask)")
-            .Define(f"Weights",f"GenWeights*ROOT::VecOps::Product(JetWeights[JetMask && JetMatchingMask])")
+            .Define(f"EvWeights",f"ROOT::VecOps::Product(JetWeights[JetMask && JetMatchingMask])")
+            .Define(f"Weights",f"GenWeights*EvWeights")
             
         )
+    
+    
     for syst in weight_syst_list:
-        dfCuts=dfCuts.Define(f"Weights_{syst}",f"GenWeights*ROOT::VecOps::Product(JetWeights_{syst}[JetMask && JetMatchingMask])")
-        #dfCuts=dfCuts.Define(f"Weights_{syst}",f"GenWeights*ROOT::VecOps::Product(JetWeights_{syst})")
+        dfCuts=(dfCuts.Define(f"EvWeights_{syst}",f"ROOT::VecOps::Product(JetWeights_{syst}[JetMask && JetMatchingMask])")
+                .Define(f"Weights_{syst}",f"GenWeights*EvWeights_{syst}")
+                )
         
-    #print(dfCuts.Mean('Weights').GetValue(),flush=True)
-
     return dfCuts
 
 def Muon_selections(rdf,dataset,syst):
@@ -64,8 +66,7 @@ def Muon_selections(rdf,dataset,syst):
             .Filter(f"Muon_pt[0]>26 && abs(Muon_eta[0])<2.4",f"{dataset}_Mu_{syst}_Muon[0]_pt>26 && abs(eta)<2.4"))
     
     dfCuts=(dfCuts.Filter(f"nJet>=4",f"{dataset}_Mu_{syst}_Clean nJet>=4")
-            #.Filter(f"Max(Jet_btagDeepFlavB)>0.2793",f"{dataset}_Mu_{syst}_Max DeepFlavB>0.2793 (Medium)")
-        )
+            )
     
     #Define new objects
     dfCuts = (dfCuts.Define(f"MET_eta", f"Met_eta(Muon_pt, Muon_eta, Muon_phi, MET_pt, MET_phi)")
@@ -92,7 +93,6 @@ def Electron_selections(rdf,dataset,syst):
             .Filter(f"Electron_pt[0]>30 && abs(Electron_eta[0])<2.4",f"{dataset}_Ele_{syst}_Electron[0]_pt>30 && abs(eta)<2.4"))
     
     dfCuts=(dfCuts.Filter(f"nJet>=4",f"{dataset}_Ele_{syst}_Clean nJet>=4")
-            .Filter(f"Max(Jet_btagDeepFlavB)>0.2793",f"{dataset}_Ele_{syst}_Max DeepFlavB>0.2793 (Medium)")
         )
     
     #Define new objects
@@ -111,6 +111,12 @@ def Electron_selections(rdf,dataset,syst):
     #report.Print()
     
     return dfCuts
+
+def btag_cuts(rdf):
+    dfCuts=(rdf.Filter(f"Max(Jet_btagDeepFlavB)>0.2793",f"Max(Jet_btagDeepFlavB)>0.2793 (Medium)")
+        )
+    return dfCuts
+
 
 import copy
 def loop_cuts(rdf_list,cuts_func,*args):
@@ -137,14 +143,34 @@ def Cut(rdf_dict,
     
     if syst!="nominal":
         weight_syst_list=[]
+        
+    
     dfCuts=loop_cuts(dfCuts,jet_selection,region.split("s")[0],weight_syst_list)
     
     sum_preWeights=sum([df.Sum("Weights").GetValue() for df in dfCuts])
-    
+
     dfCuts=loop_cuts(dfCuts,region_cut_dict[region],dataset,syst)
 
+    #!Compute weight means before btag selection
+    count=np.array([df.Count().GetValue() for df in dfCuts])
+    mu_w=np.array([df.Mean("EvWeights").GetValue() for df in dfCuts])
+    mu_w=sum(mu_w*count)/sum(count)
+    for i in range(len(dfCuts)):
+        dfCuts[i]=(dfCuts[i].Redefine("EvWeights",f"EvWeights/{mu_w}")
+                   .Redefine(f"Weights",f"GenWeights*EvWeights"))
+    sum_preWeights=sum_preWeights/mu_w
+    if syst=="nominal":
+        for weight_syst in weight_syst_list:
+            mu_wsyst=np.array([df.Mean(f"EvWeights_{weight_syst}").GetValue() for df in dfCuts])
+            mu_wsyst=sum(mu_wsyst*count)/sum(count)
+            for i in range(len(dfCuts)):
+                dfCuts[i]=(dfCuts[i].Redefine(f"EvWeights_{weight_syst}",f"EvWeights_{weight_syst}/{mu_wsyst}")
+                           .Redefine(f"Weights_{weight_syst}",f"GenWeights*EvWeights_{weight_syst}")
+                        )
+
+    dfCuts=loop_cuts(dfCuts,btag_cuts)
     sum_weights=sum([df.Sum("Weights").GetValue() for df in dfCuts])
-    
+
     eff=sum_weights/sum_preWeights
     eff_dict[region][dataset][syst]=eff
     
@@ -154,7 +180,9 @@ def Cut(rdf_dict,
     
     if syst=="nominal":
         for weight_syst in weight_syst_list:
+            
             sum_systWeights=sum([df.Sum(f"Weights_{weight_syst}").GetValue() for df in dfCuts])
+            
             eff=sum_systWeights/sum_preWeights
             eff_dict[region][dataset][weight_syst]=eff
             print(f"{weight_syst} efficiency: {eff*100:.2f}%",flush=True)

tasks/combine/systematics/root2th1.py
@@ -15,7 +15,7 @@ import gc
 cuda=torch.device("cuda:0")
 cpu=torch.device("cpu")
 
-samples_json="json/signalExample.json"
+samples_json="json/samples.json"
 bunch=1
 file_bunch_size=10
 device=cpu
@@ -29,7 +29,8 @@ sample_dict=json.load(open(samples_json,"r"))
 #! Create N rdataframes with N/bunch_size files each and concatenate them afterwards.
 
 regions=["Muons",
-         "Electrons"]
+         "Electrons"
+        ]
 
 weight_syst_list=[
            "btag_hf",