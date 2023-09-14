f49f0fded9797d57720560bb0d06b05c665c47f5
Piero <pieroviscone@gmail.com>
2023-09-14 19:04:45 +0200

    changed binning

--- tasks/combine/systematics/root2score/th1builder.py
+++ tasks/combine/systematics/root2score/th1builder.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 bins={}
-bins["Muons"]=np.concatenate((np.linspace(0,4.3,43),np.array([4.42,4.55,4.72,6.5])))
-bins["Electrons"]=np.linspace(0,5,30)
+bins["Muons"]=np.concatenate((np.linspace(0,3.588,22),np.array([3.914,4.3,6.5])))
+bins["Electrons"]=np.concatenate((np.linspace(0,2.851,14),np.array([3.289,3.728,5.5])))
 
 
 def build_TH1(score_dict,weight_dict,outfile):
@@ -30,3 +30,13 @@ def build_TH1(score_dict,weight_dict,outfile):
 
     file.Close()
 
+if __name__=="__main__":
+    import argparse
+    import torch
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--score_dict",type=str,help="Path to score dictionary")
+    parser.add_argument("--out",type=str,help="output root file")
+    args=parser.parse_args()
+    
+    score_dict=torch.load(args.score_dict)
+    build_TH1(score_dict["score_dict"],score_dict["weight_dict"],args.out)
