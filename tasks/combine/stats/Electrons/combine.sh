5178d87bf9117bb1a6b141a0962dfa177a6b24f4
Piero <pieroviscone@gmail.com>
2023-09-19 15:20:40 +0200

    removed stats folder

--- tasks/combine/stats/Electrons/combine.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-text2workspace.py $1 -o workspace_$2.root  | tee log/workspace_$2.out
-
-
-combine workspace_$2.root -M MultiDimFit --algo grid --points 2000 -t -1 --expectSignal=1 --saveFitResult --setParameterRanges r=0.8,1.2   | tee log/Electrons_$2.out
-mv higgsCombineTest.MultiDimFit.mH120.root ./results/Electrons_$2.root
-
--- tasks/combine/stats/Electrons/run.sh
+++
@@ -1,7 +0,0 @@
-echo "Remember to deactivate the conda enviroment"
-rm results/*
-
-./combine.sh datacard_new.txt new_likelihood
-./combine.sh datacard_old.txt old_likelihood
-
-
